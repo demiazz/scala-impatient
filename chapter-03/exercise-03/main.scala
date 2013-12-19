104511042f91ce146bab5a1531868a67b73c8441
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-19 13:12:39 +0400

    Fix solution for 03/03

--- chapter-03/exercise-03/main.scala
+++ chapter-03/exercise-03/main.scala
@@ -1,7 +1,7 @@
 val a = Array(1, 2, 3, 4, 5)
 val b = for (i <- 0 until a.length) yield {
   if (i % 2 == 0) {
-    if (i != a.length - 1) a(i + 1) else a(i)
+    if (i < a.length - 1) a(i + 1) else a(i)
   } else {
     a(i - 1)
   }
