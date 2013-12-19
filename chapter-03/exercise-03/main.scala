fbcb540765c5c32c86b98aff2c91f43926f1ef28
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-19 12:38:38 +0400

    Add solution for 03/03

---
+++ chapter-03/exercise-03/main.scala
@@ -0,0 +1,10 @@
+val a = Array(1, 2, 3, 4, 5)
+val b = for (i <- 0 until a.length) yield {
+  if (i % 2 == 0) {
+    if (i != a.length - 1) a(i + 1) else a(i)
+  } else {
+    a(i - 1)
+  }
+}
+
+println(b.mkString(", "))
