b0f152634a5c0e4bbb6aa314419763f92a04221e
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-24 01:13:16 +0400

    Add solution for 03/10

---
+++ chapter-03/exercise-10/main.scala
@@ -0,0 +1,13 @@
+import java.awt.datatransfer._
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.mutable.Buffer
+
+val flavors = SystemFlavorMap.getDefaultFlavorMap()
+                             .asInstanceOf[SystemFlavorMap]
+
+val natives: Buffer[String]  = flavors
+                                 .getNativesForFlavor(DataFlavor.imageFlavor)
+
+for (native <- natives) println(native)
+
+
