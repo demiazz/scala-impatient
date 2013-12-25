86f3e2e5c5e1a56211e5a519d63b46b41efa437a
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-25 17:59:53 +0400

    Fix solution for 04/03

--- chapter-04/exercise-03/main.scala
+++ chapter-04/exercise-03/main.scala
@@ -1,19 +1,14 @@
 import java.util.Scanner
 import java.io.File
-import scala.collection.immutable.HashMap
 
-val in = new Scanner(new File("example.txt"))
+val in     = new Scanner(new File("example.txt"))
+var counts = Map[String, Int]() withDefault(_ => 0)
 
-var counts = new HashMap[String, Int]
+while (in hasNext) {
+  val key          = in next
+  val currentCount = counts(key)
 
-while (in.hasNext()) {
-  val word = in.next()
-
-  if (counts.contains(word)) {
-    counts = counts + (word -> (counts(word) + 1))
-  } else {
-    counts = counts + (word -> 1)
-  }
+  counts = counts - key + (key -> (currentCount + 1))
 }
 
 for ((word, count) <- counts) println(word + ": " + count)
