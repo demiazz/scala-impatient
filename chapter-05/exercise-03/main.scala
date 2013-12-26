15d4eaec7e13eaf86e1bbc0d3a3028d9f799ea61
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-26 14:15:35 +0400

    Add solution for 05/03

---
+++ chapter-05/exercise-03/main.scala
@@ -0,0 +1,17 @@
+class Time(val hrs: Int, val min: Int) {
+
+  def before(other: Time): Boolean = {
+    hrs < other.hrs || (hrs == other.hrs && min < other.min)
+  }
+
+  override def toString = "%02d:%02d".format(hrs, min)
+
+}
+
+val morning      = new Time(8, 0)
+val noon         = new Time(12, 0)
+val noonWithHalf = new Time(12, 30)
+
+println(morning + " before than " + noon         + " => " + morning.before(noon))
+println(noon    + " before than " + morning      + " => " + noon.before(morning))
+println(noon    + " before than " + noonWithHalf + " => " + noon.before(noonWithHalf))
