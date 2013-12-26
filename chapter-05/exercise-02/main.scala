316dadabe9e16edcb5bb3f750320514b6e254a23
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-26 13:59:22 +0400

    Add solution for 05/02

---
+++ chapter-05/exercise-02/main.scala
@@ -0,0 +1,25 @@
+class BankAccount {
+
+  private var _balance = 0.0
+
+  def deposit(money: Double) {
+    _balance += money
+  }
+
+  def withdraw(money: Double) {
+    _balance -= money
+  }
+
+  def balance = _balance
+
+  override def toString = "Balance: " + balance
+
+}
+
+val account = new BankAccount
+
+println(account)
+account.deposit(20000)
+println(account)
+account.withdraw(10000)
+println(account)
