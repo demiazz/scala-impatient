807492974013902ea1e1681305f57f10110f13c7
Alexey Plutalov <demiazz.py@gmail.com>
2013-12-26 13:46:02 +0400

    Fix solution for 05/01

--- chapter-05/exercise-01/main.scala
+++ chapter-05/exercise-01/main.scala
@@ -1,16 +1,18 @@
 class Counter {
-  private var value = 0
+  private var value = Int.MaxValue - 1
 
-  def increment() {
-    if (value < Int.MaxValue) value += 1
-  }
+  def increment() = if (value < Int.MaxValue) value += 1
 
-  def current()   = value
+  def current = value
+
+  override def toString = "Counter: " + current
 }
 
 val counter = new Counter
 
-for (i <- 0 until Int.MaxValue) counter.increment()
-for (i <- 0 to 10)           counter.increment()
+println(counter)
 
-println("Current value of counter: " + counter.current())
+for (i <- 0 to 2) {
+  counter.increment()
+  println(counter)
+}
