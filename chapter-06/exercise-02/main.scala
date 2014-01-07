5627fa842aea9ef34918e9041cd699abab579bde
Alexey Plutalov <demiazz.py@gmail.com>
2014-01-07 05:15:39 +0400

    Fix for solution 06/02

--- chapter-06/exercise-02/main.scala
+++ chapter-06/exercise-02/main.scala
@@ -1,22 +1,12 @@
-class UnitConversion {
-
-  protected val factor: Double = 1.0;
+class UnitConversion(protected val factor: Double = 1.0) {
 
   def convert(value: Double) = value * factor
 
 }
 
-object InchesToCentimeters extends UnitConversion {
-  override protected val factor = 2.54
-}
-
-object GallonsToLiters extends UnitConversion {
-  override protected val factor = 3.78541178
-}
-
-object MilesToKilometers extends UnitConversion {
-  override protected val factor = 1.609344
-}
+object InchesToCentimeters extends UnitConversion(2.54)
+object GallonsToLiters     extends UnitConversion(3.78541178)
+object MilesToKilometers   extends UnitConversion(1.609344)
 
 println(InchesToCentimeters.convert(1.0) + " centimeters")
 println(InchesToCentimeters.convert(5.0) + " centimeters")
