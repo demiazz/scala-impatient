6741bc7d914fe49ba25f929b24e0855dc4d63422
Alexey Plutalov <demiazz.py@gmail.com>
2014-01-07 05:51:30 +0400

    Add solution for 06/08

---
+++ chapter-06/exercise-08/main.scala
@@ -0,0 +1,16 @@
+object RGBCubeCorners extends Enumeration {
+
+  val Black   = Value(0x000000)
+  val Red     = Value(0xff0000)
+  val Green   = Value(0x00ff00)
+  val Blue    = Value(0x0000ff)
+  val Yellow  = Value(0xffff00)
+  val Cyan    = Value(0x00ffff)
+  val Magenta = Value(0xff00ff)
+  val White   = Value(0xffffff)
+
+}
+
+for (color <- RGBCubeCorners.values) {
+  println("0x%06x".format(0xffffff & color.id) + " is " + color)
+}
